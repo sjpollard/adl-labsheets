2c2bff4c47bdba7f76bb4718f769b0660710e857
sjpollard <samueljosephpollard@gmail.com>
2022-10-21 08:58:04 +0100

    Lab 2

--- lab-2-cnns/lab2-code/train_cifar.sh
+++ lab-2-cnns/lab2-code/train_cifar.sh
@@ -3,8 +3,7 @@
 #SBATCH --job-name=lab2
 #SBATCH --partition=teach_gpu
 #SBATCH --nodes=1
-#SBATCH -o ./log_%j.out # STDOUT out
-#SBATCH -e ./log_%j.err # STDERR out
+#SBATCH --output train_cifar.out
 #SBATCH --gres=gpu:1
 #SBATCH --time=0:20:00
 #SBATCH --mem=4GB
@@ -14,4 +13,4 @@ module purge
 # load in the module dependencies for this script
 module load "languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch"
 
-python train_cifar.py
+python train_cifar.py --learning-rate 0.001
