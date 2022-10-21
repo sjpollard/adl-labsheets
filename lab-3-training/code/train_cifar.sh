621aa3fb37f584965c0470e0755e5fa9dde3a0ed
sjpollard <samueljosephpollard@gmail.com>
2022-10-21 11:46:03 +0100

    Lab 3

---
+++ lab-3-training/code/train_cifar.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=lab3
+#SBATCH --partition=teach_gpu
+#SBATCH --nodes=1
+#SBATCH --output train_cifar.out
+#SBATCH --gres=gpu:1
+#SBATCH --time=0:20:00
+#SBATCH --mem=4GB
+
+# get rid of any modules already loaded
+module purge
+# load in the module dependencies for this script
+module load "languages/anaconda3/2021-3.8.8-cuda-11.1-pytorch"
+
+python train_cifar.py --learning-rate 1e-1 --batch-size 128 --sgd-momentum 1.8
